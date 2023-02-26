5ce10b2c3cd688744e695b286920ba9aac46bfe3
vallis20 <vallisguerrero32@gmail.com>
2023-02-26 12:33:32 -0600

    holiwis

---
+++ src/formulario.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import MeditokenApp from "./MeditokenApp";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Login Form</h1>
+        <MeditokenApp />
+      </div>
+    );
+  }
+}
